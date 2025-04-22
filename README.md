0564142b74153bb7342a5db4b53587ce7fc21191
André Luis Zitelli Rettondini <andre.retton21@gmail.com>
2025-04-22 13:32:27 -0300

    finished project

---
+++ README.md
@@ -0,0 +1,2 @@
+# T1-Compiladores
+Trabalho 1 da disciplina de compiladores
---
+++ src/main/java/br/ufscar/dc/compiladores/Main.java
@@ -0,0 +1,49 @@
+package br.ufscar.dc.compiladores;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        JanderLexico lex = new JanderLexico(cs);
+        String arquivoSaida = args[1];
+        PrintWriter pw = new PrintWriter(arquivoSaida);
+
+        Token t = null;
+
+        while((t = lex.nextToken()).getType() != Token.EOF) {
+            String nomeToken = JanderLexico.VOCABULARY.getDisplayName(t.getType());
+
+            if(nomeToken.equals("ERRO")) {
+                    //System.out.println("Erro na linha "+t.getLine()+": "+t.getText());
+                    pw.println("Erro na linha "+t.getLine()+": "+t.getText());
+                    break;
+                } else if(nomeToken.equals("CADEIA_NAO_FECHADA")) {
+                    //System.out.println("Cadeia não fechada na linha "+t.getLine());
+                    pw.println("Cadeia não fechada na linha "+t.getLine());
+                    break;
+                } else if(nomeToken.equals("COMENTARIO_NAO_FECHADO")) {
+                    //System.out.println("Cadeia não fechada na linha "+t.getLine());
+                    pw.println("Comentário não fechado na linha "+t.getLine());
+                    break;
+                } else if(nomeToken.equals("OP_REL" ) || nomeToken.equals("OP_ARIT" ) || nomeToken.equals("OP_LOGICO")){
+                // System.out.println("<" + '\'' + t.getText() + '\'' + ',' + '\'' + t.getText() + '\'' + ">");
+                pw.println("<" + '\'' + t.getText() + '\'' + ',' + '\'' + t.getText() + '\'' + ">");
+            } else {
+                    //System.out.println("<" + nomeToken + "," + t.getText() + ">");
+                    pw.println("<" + nomeToken + "," + t.getText() + ">");
+                }
+        }
+
+        pw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
